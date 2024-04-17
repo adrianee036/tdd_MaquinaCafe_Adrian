bab0d107c467065e2bf7ed3eb4afffdb8c81f063
adrianee036 <20212225@itla.edu.do>
2024-04-17 19:31:50 -0400

    Update

--- implementacion/maquinacafe.py
+++ implementacion/maquinacafe.py
@@ -1,13 +1,87 @@
+from implementacion.vaso import Vaso
+from implementacion.cafetera import Cafetera
+from implementacion.azucarero import Azucarero
+
 class MaquinaCafe:
-    vasoPequeno = ""
 
-    def __init__(self, cafe, vasosPequenos, vasosMedianos, vasosGrandes, azucar):
-        self.cafe = cafe
+    cafe = Cafetera(0)
+    vasosPequenos = Vaso(0,0)
+    vasosMedianos = Vaso(0,0)
+    vasosGrandes = Vaso(0,0)
+    azucar = Azucarero(0)
+
+
+    def setCafetera(self, cantidadCafe):
+        self.cafe = cantidadCafe
+    
+    def setVasosPequeno(self, vasosPequenos):
         self.vasosPequenos = vasosPequenos
-        self.vasosMedianos = vasosMedianos
-        self.vasosGrandes = vasosGrandes
-        self.azucar = azucar
 
-    def setCafetera(self, vasosPequenos):
-        self.vasoPequeno = vasosPequenos
+    def setVasosMediano(self, vasosMedianos):
+        self.vasosMedianos = vasosMedianos
+    
+    def setVasosGrande(self, vasosGrandes):
+        self.vasosGrandes = vasosGrandes
+
+    def setAzucarero(self, cantidadAzucar):
+        self.azucar = cantidadAzucar
+    
+    
+    def getCafetera(self):
+        return self.cafe
+    
+    def getVasosPequeno(self):
+        return self.vasosPequenos
+    
+    def getAzucarero(self):
+        return self.azucar
+    
+    def getTipoDeVaso(self, tipo):
+        if tipo == "pequeno":
+            return self.vasosPequenos
+        elif tipo == "mediano":
+            return self.vasosMedianos
+        elif tipo == "grande":
+            return self.vasosGrandes
+        else:
+            return None
+    
+    def getVasoDeCafe(self, vaso, cantidadDeVasos, cantidadDeAzucar):        
+
+        if self.vasosPequenos == vaso:
+            if cantidadDeVasos > self.vasosPequenos.cantidadVasos:
+                return "No hay Vasos"
+            elif self.cafe.cantidadCafe < self.vasosPequenos.contenido:
+                return "No hay Cafe"
+            elif cantidadDeAzucar > self.azucar.cantidadDeAzucar:
+                return "No hay Azucar" 
+            else: 
+                self.cafe.cantidadCafe =  self.cafe.cantidadCafe - self.vasosPequenos.contenido
+                self.vasosPequenos.cantidadVasos =  self.vasosPequenos.cantidadVasos - cantidadDeVasos
+                self.azucar.cantidadDeAzucar =  self.azucar.cantidadDeAzucar - cantidadDeAzucar
+                return "Felicitaciones"
+            
+        elif self.vasosMedianos == vaso:
+            if cantidadDeVasos > self.vasosMedianos.cantidadVasos:
+                return "No hay Vasos"
+            elif self.cafe.cantidadCafe < self.vasosMedianos.contenido:
+                return "No hay Cafe"
+            elif cantidadDeAzucar > self.azucar.cantidadDeAzucar:
+                return "No hay Azucar" 
+                
+            
+        elif self.vasosGrandes == vaso:
+            if cantidadDeVasos > self.vasosGrandes.cantidadVasos:
+                return "No hay Vasos"
+            elif self.cafe.cantidadCafe < self.vasosGrandes.contenido:
+                return "No hay Cafe"
+            elif cantidadDeAzucar > self.azucar.cantidadDeAzucar:
+                return "No hay Azucar" 
+            
+        
+            
+
+    
+    
+
 
--- tests/test_maquina_cafe.py
+++ tests/test_maquina_cafe.py
@@ -22,7 +22,94 @@ class TestMaquinaDeCafe(unittest.TestCase):
 
         self.maquinaDeCafe = MaquinaCafe()
         self.maquinaDeCafe.setCafetera(self.cafetera)
+        self.maquinaDeCafe.setVasosPequeno(self.vasosPequeno)
+        self.maquinaDeCafe.setVasosMediano(self.vasosMediano)
+        self.maquinaDeCafe.setVasosGrande(self.vasosGrande)
+        self.maquinaDeCafe.setAzucarero(self.azucarero)
 
+    def test_DeberiaDevolverUnVasoPequeno(self) -> None:
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+
+        self.assertEquals(self.maquinaDeCafe.vasosPequenos,vaso)
+
+    def test_DeberiaDevolverUnVasoMediano(self) -> None:
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("mediano")
+
+        self.assertEquals(self.maquinaDeCafe.vasosMedianos,vaso)
+    
+    def test_DeberiaDevolverUnVasoGrande(self) -> None:
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("grande")
+
+        self.assertEquals(self.maquinaDeCafe.vasosGrandes,vaso)
+
+    def test_DeberiaDevolverNoHayVasos(self) -> None:
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+
+        resultado = self.maquinaDeCafe.getVasoDeCafe(vaso,10,2)
+
+        self.assertEquals("No hay Vasos",resultado)
+
+    def test_DeberiaDevolverNoHayCafe(self) -> None:
+
+        cafetera = Cafetera(5)
+        self.maquinaDeCafe.setCafetera(cafetera)
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+        
+        resultado = self.maquinaDeCafe.getVasoDeCafe(vaso,1,2)
+
+        self.assertEquals("No hay Cafe",resultado)
+
+    def test_DeberiaDevolverNoHayAzucar(self) -> None:
+
+        self.azucarero = Azucarero(2)
+        self.maquinaDeCafe.setAzucarero(self.azucarero)
+
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+        
+        resultado = self.maquinaDeCafe.getVasoDeCafe(vaso,1,3)
+
+        self.assertEquals("No hay Azucar",resultado) 
+    
+    def test_DeberiaRestarCafe(self) -> None:
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+        
+        self.maquinaDeCafe.getVasoDeCafe(vaso,1,3)
+
+        resultado = self.maquinaDeCafe.getCafetera().getCantidadCafe()
+
+        self.assertEquals(40,resultado) 
+    
+    def test_DeberiaRestarVaso(self) -> None:
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+        
+        self.maquinaDeCafe.getVasoDeCafe(vaso,1,3)
+
+        resultado = self.maquinaDeCafe.getVasosPequeno().getCantidadVasos()
+
+        self.assertEquals(4,resultado) 
+
+    def test_DeberiaRestarAzucar(self) -> None:
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+        
+        self.maquinaDeCafe.getVasoDeCafe(vaso,1,3)
+
+        resultado = self.maquinaDeCafe.getAzucarero().getCantidadAzucar()
+
+        self.assertEquals(17,resultado)
+
+    def test_DeberiaDevolverFelicitaciones(self) -> None:
+        vaso = self.maquinaDeCafe.getTipoDeVaso("pequeno")
+
+        resultado = self.maquinaDeCafe.getVasoDeCafe(vaso,1,3)
+
+        self.assertEquals("Felicitaciones",resultado) 
+
+    
 
 
 
